50b0c4f6c87993910427f90ea4a498531a551ea2
Aleskey <colthands@protonmail.com>
2024-12-02 00:37:24 +0400

    day 1 part 2

--- src/advent_of_code_2024.rs
+++ src/advent_of_code_2024.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::Read;
+use std::collections::HashMap;
 
 pub fn read_file(path: String) -> String {
     let mut file = File::open(path).expect("Unable to open file");
@@ -10,7 +11,7 @@ pub fn read_file(path: String) -> String {
     return contents;
 }
 
-pub fn day1() {
+pub fn day1_contents() -> (Vec<i32>, Vec<i32>) {
     let contents = read_file("inputs/day1.txt".to_string());
     let contents_parsed = contents
         .split("\r\n")
@@ -25,6 +26,12 @@ pub fn day1() {
         arr2.push(pair[1].parse::<i32>().unwrap());
     }
 
+    return (arr1, arr2);
+}
+
+pub fn day1_pt1() -> i32 {
+    let (mut arr1, mut arr2) = day1_contents();
+
     arr1.sort();
     arr2.sort();
 
@@ -34,5 +41,29 @@ pub fn day1() {
         diff += (arr1[i] - arr2[i]).abs();
     }
 
-    println!("{:?}", (diff));
+    return diff;
+}
+
+pub fn day1_pt2() -> i32 {
+    let mut count_map = HashMap::new();
+
+    let (mut arr1, arr2) = day1_contents();
+
+    for i in 0..arr2.len() {
+        if count_map.contains_key(&arr2[i]) {
+            count_map.insert(arr2[i], count_map[&arr2[i]] + 1);
+        } else {
+            count_map.insert(arr2[i], 1);
+        }
+    }
+
+    let mut total: i32 = 0;
+
+    for num in arr1.iter_mut() {
+        if count_map.contains_key(&num) {
+            total += *num * count_map[num];
+        }
+    }
+
+    return total;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 mod advent_of_code_2024;
+use advent_of_code_2024::{
+    day1_pt1,
+    day1_pt2
+};
 
 fn main() {
-    println!("Hello, world!");
-    advent_of_code_2024::day1();
+    log!("Day 1 result", day1_pt1());
+    log!("Day 1 result", day1_pt2());
+}
+
+#[macro_export]
+macro_rules! log {
+    ($($arg:tt)*) => {
+        println!("{:?}", ($($arg)*));
+    }
 }
