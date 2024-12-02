e9e20f663b691bfe62c5a6cebe92773184a68cb6
Aleksei Karpenko <akarpenko@evolution.com>
2024-12-02 17:20:43 +0400

    day 1 part 1 and slow part 2

--- src/advent_of_code_2024.rs
+++ src/advent_of_code_2024.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::Read;
 use std::collections::HashMap;
 
-pub fn read_file(path: String) -> String {
+fn read_file(path: String) -> String {
     let mut file = File::open(path).expect("Unable to open file");
     let mut contents = String::new();
 
@@ -11,7 +11,75 @@ pub fn read_file(path: String) -> String {
     return contents;
 }
 
-pub fn day1_contents() -> (Vec<i32>, Vec<i32>) {
+fn parse_contents_by_line_by_word_as_integer(path: String) -> Vec<Vec<i32>> {
+    let contents = read_file(path);
+    let contents_parsed = contents
+        .lines()
+        .map(|line| {
+            line
+                .split_whitespace()
+                .map(|word| word.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect::<Vec<Vec<i32>>>();
+
+    return contents_parsed
+}
+
+pub fn day2_pt1() -> i32 {
+    let contents = parse_contents_by_line_by_word_as_integer("inputs/day2.txt".to_string());
+
+    let mut safe_count = 0;
+
+    for line in contents {
+        safe_count += if is_line_safe(&line) { 1 } else { 0 };
+    }
+
+    return safe_count;
+}
+
+pub fn day2_pt2() -> usize {
+    let contents = parse_contents_by_line_by_word_as_integer("inputs/day2.txt".to_string());
+
+    let mut safe_count = 0;
+
+    for line in contents {
+        if is_line_safe(&line) {
+            safe_count += 1;
+        } else {
+            for i in 0..line.len() {
+                let mut modified_line = line.clone();
+                modified_line.remove(i);
+                if is_line_safe(&modified_line) {
+                    safe_count += 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    return safe_count;
+}
+
+fn is_line_safe(line: &Vec<i32>) -> bool {
+    let is_decreasing = line[0] > line[1];
+
+    for i in 1..line.len() {
+        if (line[i-1] > line[i]) != is_decreasing {
+            return false;
+        }
+
+        let diff = (line[i] - line[i - 1]).abs();
+
+        if diff < 1 || diff > 3 {
+            return false
+        }
+    }
+
+    return true;
+}
+
+fn day1_contents() -> (Vec<i32>, Vec<i32>) {
     let contents = read_file("inputs/day1.txt".to_string());
     let contents_parsed = contents
         .split("\r\n")
@@ -29,22 +97,7 @@ pub fn day1_contents() -> (Vec<i32>, Vec<i32>) {
     return (arr1, arr2);
 }
 
-pub fn day1_pt1() -> i32 {
-    let (mut arr1, mut arr2) = day1_contents();
-
-    arr1.sort();
-    arr2.sort();
-
-    let mut diff = 0;
-
-    for i in 0..arr1.len() {
-        diff += (arr1[i] - arr2[i]).abs();
-    }
-
-    return diff;
-}
-
-pub fn day1_pt2() -> i32 {
+fn day1_pt2() -> i32 {
     let mut count_map = HashMap::new();
 
     let (mut arr1, arr2) = day1_contents();
@@ -66,4 +119,19 @@ pub fn day1_pt2() -> i32 {
     }
 
     return total;
+}
+
+fn day1_pt1() -> i32 {
+    let (mut arr1, mut arr2) = day1_contents();
+
+    arr1.sort();
+    arr2.sort();
+
+    let mut diff = 0;
+
+    for i in 0..arr1.len() {
+        diff += (arr1[i] - arr2[i]).abs();
+    }
+
+    return diff;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 mod advent_of_code_2024;
 use advent_of_code_2024::{
-    day1_pt1,
-    day1_pt2
+    // day1_pt1,
+    // day1_pt2,
+    day2_pt1,
+    day2_pt2,
 };
 
 fn main() {
-    log!("Day 1 result", day1_pt1());
-    log!("Day 1 result", day1_pt2());
+    // log!("Day 1 pt 1 result", day1_pt1());
+    // log!("Day 1 pt 2 result", day1_pt2());
+
+    log!("Day 2 pt 1 result", day2_pt1());
+    log!("Day 2 pt 2 result", day2_pt2());
 }
 
 #[macro_export]
