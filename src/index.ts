397b996fea8fcda64062968d2815fe2305947450
Aleskey <colthands@protonmail.com>
2024-12-07 17:50:24 +0400

    day 7 + tsx dep

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,53 @@
 import fs from "node:fs"
 
 class AdventOfCode2024 {
-    private inputs_folder = 'inputs'
+    private static inputs_folder = 'inputs'
 
-    day3_contents() {
+    static day7_contents() {
+        const contents = fs.readFileSync(`${AdventOfCode2024.inputs_folder}/day7.txt`, 'utf8')
+
+        return contents
+    }
+
+    static day7() {
+        const contents = AdventOfCode2024
+            .day7_contents()
+            .split('\n')
+            .map(line => {
+                const lineS = line
+                .trim()
+                .split(':')
+                .map(str => str.trim())
+
+                return [parseInt(lineS[0]), lineS[1].split(' ').map(Number)]
+            })
+            .filter(([goal, numbers]) => dfs([goal, numbers]))
+            .reduce((acc, [goal]) => acc + goal, 0)
+
+        function dfs ([goal, numbers], curr_result = 0) {
+            if(numbers.length === 0) {
+                return curr_result === goal
+            }
+
+            const [number, ...rest] = numbers
+            const gr = [goal, rest]
+            const m = curr_result * number
+            const a = curr_result + number
+            const c = Number(`${curr_result}${number}`)
+
+            return dfs(gr, a) || dfs(gr, m) || dfs(gr, c)
+        }
+
+        console.log('contents', contents)
+    }
+
+    static day3_contents() {
         const contents = fs.readFileSync(`${this.inputs_folder}/day3.txt`, 'utf8')
 
         return contents
     }
 
-    day3_pt1() {
+    static day3_pt1() {
         const input = this.day3_contents()
         const regex = /mul\(\d{1,3},\d{1,3}\)/g
         const mul = (n, m) => n * m
@@ -18,7 +56,7 @@ class AdventOfCode2024 {
         console.log('sum', sum)
     }
 
-    day3_pt2() {
+    static day3_pt2() {
         const input = this.day3_contents()
         const regex = /mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/g
         const mul = (n, m) => n * m
@@ -43,7 +81,4 @@ class AdventOfCode2024 {
     }
 }
 
-const aoc2024 = new AdventOfCode2024()
-
-aoc2024.day3_pt1()
-aoc2024.day3_pt2()
+AdventOfCode2024.day7()
